499738f0dbf6d061528b9d4700b0b164c70f9031
Kyle van den Boom <520163@student.glu.nl>
2017-12-04 09:32:20 +0100

    Kyle is back at it again fam.

---
+++ Project6Ronimo/Assets/Scripts/Kyle/UnitStats.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public enum UnitRange
+    {
+        Melee,
+        Ranged
+    };
+
+    public enum UnitType
+    {
+        Collector,
+        SwordMaster,
+        Spellcaster,
+        Archer,
+        //SpecialUnit,
+    };
+
+    private UnitRange m_unitrange;
+    private UnitType m_unittype;
+    int m_health;
+    int m_damage;
+    float m_movespeed;
+    int m_goldcost;
+
+    public void SetRange(UnitRange range)
+    {
+        m_unitrange = range;
+    }
+
+    public UnitRange GetUnitRange()
+    {
+        return m_unitrange;
+    }
+
+    public void SetUnit(UnitType type)
+    {
+        m_unittype = type;
+    }
+
+    public UnitType GetUnitType()
+    {
+        return m_unittype;
+    }
+
+    public void SetHealth(int amount)
+    {
+        m_health = amount;
+    }
+
+    public int GetUnitHealth()
+    {
+        return m_health;
+    }
+
+    public void SetDamage(int amount)
+    {
+        m_damage = amount;
+    }
+
+    public int GetUnitDamage()
+    {
+        return m_damage;
+    }
+
+    public void SetMovespeed(float amount)
+    {
+        m_movespeed = amount;
+    }
+
+    public float GetMovespeed()
+    {
+        return m_movespeed;
+    }
+
+    public void SetGoldCost(int amount)
+    {
+        m_goldcost = amount;
+    }
+
+    public int GetGoldCost()
+    {
+        return m_goldcost;
+    }
+}
