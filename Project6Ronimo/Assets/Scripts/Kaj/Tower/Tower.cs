f63125b5f7163403f992ae50a7fc83148bc26354
Kaj Rumpff <rumpff.kaj@gmail.com>
2017-12-04 12:40:46 +0100

    Tower, nog niet af

---
+++ Project6Ronimo/Assets/Scripts/Kaj/Tower/Tower.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    #region Variable initalization
+    [SerializeField]
+    private TowerAsset m_towerAsset;
+    private float m_attackRadius; //The radius in which a enemy has to be before the tower can shoot at it
+    private float m_cooldown; //The attack cooldown, in seconds
+    private int m_health;
+
+    private ITowerState m_currentState; // Holds the current state
+    #endregion
+
+    #region Unity Functions
+    private void Start()
+    {        
+        m_attackRadius = m_towerAsset.AttackRadius;
+        m_cooldown = m_towerAsset.Cooldown;
+        m_health = m_towerAsset.Heath;
+        m_currentState = new TowerStateAttack(this);
+    }
+
+    private void Update()  
+    {
+        m_currentState.Update(); // Updates the current state
+
+        // Update things that always need to be updated
+        if (m_health > 0)
+            Destroy();
+    }
+    #endregion
+
+    public void Shoot()
+    {
+        Debug.Log("Piew!");
+    }
+
+    public void SwitchState(ITowerState state) // Switch the state of the tower, give new class of the state
+    {
+        m_currentState.End();   // Call the end of the current state
+        m_currentState = state; // Switch the state
+        m_currentState.Start(); // Call the start of the new state
+    }
+
+    public void RecieveDamage(int amount)
+    {
+        m_health -= amount;
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    #region Get/Set
+    public float Cooldown
+    {
+        get { return m_cooldown; }
+        set { m_cooldown = value; }
+    }
+    public float AttackRadius
+    {
+        get { return m_attackRadius; }
+        set { m_attackRadius = value; }
+    }
+    public TowerAsset TowerAsset
+    {
+        get { return m_towerAsset; }
+    }
+    #endregion
+}
---
+++ Project6Ronimo/Assets/Scripts/Kaj/Tower/TowerBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITowerState
+{
+    void Start();   // Is called at the first frame of the state
+    void Update();  // Is called every frame when the state is active
+    void End();     // Is called at the last frame of the state
+}
+
+public class TowerStateIdle : ITowerState
+{
+    private Tower m_tower; // The tower
+
+    public TowerStateIdle(Tower tower)
+    {
+        m_tower = tower;
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void End()
+    {
+
+    }
+}
+
+public class TowerStateAttack : ITowerState
+{
+    private Tower m_tower; // The tower
+    private float m_cooldown; // The time the tower takes to 'cool down'
+    private float m_cooldownTimer; // The current time in the cooldown
+
+    public TowerStateAttack(Tower tower)
+    {
+        m_tower = tower;
+        m_cooldown = m_tower.Cooldown;
+        m_cooldownTimer = 0;
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        if (m_cooldownTimer > m_cooldown)   // Check if the tower is ready to shoot
+        {
+            m_tower.Shoot();        // Tell the tower to shoot
+            m_cooldownTimer = 0;    // Reset the cooldown timer
+        }
+        
+        m_cooldownTimer += Time.deltaTime;  // Time the cooldown timer
+    }
+
+    public void End()
+    {
+
+    }
+}
